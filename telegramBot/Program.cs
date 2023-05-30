78c076596b37d1a11dfa484aa95eba9c80d79fb5
vorgnomov <sakolinskayaveronika@mail.ru>
2023-05-30 11:00:29 +0300

    Initial commit

---
+++ telegramBot/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+using Telegram.Bots.Requests;
+using Telegram.Bots.Types;
+using InlineKeyboardMarkup = Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup;
+using Update = Telegram.Bot.Types.Update;
+using UpdateType = Telegram.Bot.Types.Enums.UpdateType;
+
+
+namespace telegramBot
+{
+
+    class Program
+    {
+        public static string language;
+        static ITelegramBotClient bot = new TelegramBotClient("5854774014:AAGf6H0PwyQTjOAiTJ3noekH3WKs2l1_kRI");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            var message = update.Message;
+            if(update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                if (message.Text.ToLower() == "/start")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Приветик. Введите текст который Вы желаете перевести)");
+                    return;
+                }
+                if (message.Text.ToLower() == "/about")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "Это бот переводчик. Он может переводить текст с русского на англиский и наоборот");
+                    return;
+                }
+
+                if (message.Text.ToLower() == "/help")
+                {
+                    await botClient.SendTextMessageAsync(message.Chat, "help");
+                    return;
+                }
+                if (update.Type != UpdateType.Message)
+                    return;
+
+                message = update.Message;
+
+                if (message == null || message.Type != MessageType.Text)
+                    return;
+
+                Console.WriteLine($"Received a text message in chat {message.Chat.Id}.");
+                /*await botClient.SendTextMessageAsync(
+                    chatId: message.Chat.Id,
+                    text: "На какой язык Вы желаете перевести?"
+                );*/
+                TranslateButtons(botClient, update, cancellationToken);
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Активизирован бот " + bot.GetMeAsync().Result.FirstName);
+
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { },
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+
+        public static async void TranslateText(ITelegramBotClient botClient, Update update,
+            CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+        }
+
+        public static async void TranslateButtons(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            var inlineKeyboard = new InlineKeyboardMarkup(new[]
+                {
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData("Англиский", "english"),
+                    },
+                    new []
+                    {
+                        InlineKeyboardButton.WithCallbackData("Русский", "russian"),
+                    }
+                }); 
+            await bot.SendTextMessageAsync(message.Chat.Id, "На какой язык перевести?", replyMarkup: inlineKeyboard);
+            return;
+        }
+    }
+}
