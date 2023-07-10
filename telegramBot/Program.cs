5565de88d7801dc8fa987d960800b53554632084
vorgnomov <sakolinskayaveronika@mail.ru>
2023-07-10 16:07:03 +0300

    fix timer

--- telegramBot/Program.cs
+++ telegramBot/Program.cs
@@ -85,7 +85,7 @@ namespace telegramBot
             var botClient = new TelegramBotClient("5991659123:AAHSfX4vBRKa6abDFzPFXScmyTBN7yOBQog");
             var cancelToken = new CancellationToken();
             DateTime currentTime = DateTime.Now;
-            if (currentTime.Hour == 15 && currentTime.Minute == 58)
+            if (currentTime.Hour == 7 && currentTime.Minute == 00)
             {
                 Spam(botClient, update, cancelToken);
             }
